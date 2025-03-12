55e3aa41354003742aeee05f3b328b3b4d8ab60f
vantecc <evansauditore@gmail.com>
2025-03-12 15:44:42 -0300

    Aplicativo teste para receita de comidas

---
+++ screens/SplashScreen.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function SplashScreen({ navigation }) {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('Home');
+    }, 3000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>SplashScreen Funcionando ✅</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'yellow', 
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
